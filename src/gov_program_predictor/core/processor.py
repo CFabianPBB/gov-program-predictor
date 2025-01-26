80c903dae6f5a843298f4a8747037cfcb045459d
Christopher Fabian <christopherfabian@MacBook-Pro-102.local>
2025-01-26 13:27:17 -0700

    Switch to GPT-3.5-turbo-16k

--- src/gov_program_predictor/core/processor.py
+++ src/gov_program_predictor/core/processor.py
@@ -12,7 +12,7 @@ import re
 load_dotenv()
 
 class ProgramPredictor:
-    def __init__(self, model_name="gpt-4-32k"):
+    def __init__(self, model_name="gpt-3.5-turbo-16k"):
         self.llm = ChatOpenAI(
             model=model_name,
             temperature=0.7
